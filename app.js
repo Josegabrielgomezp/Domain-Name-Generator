cea4be30b2fc2ebc3d50f85070087b3a9e9b74b3
Josegabrielgomezp <Code1801gp@gmail.com>
2022-10-08 00:03:46 +0000

    Domain Name Generator done

---
+++ app.js
@@ -0,0 +1,9 @@
+let prefix = ["Amazing", "Cool", "Big", "Super", "Very", "Inmportant"];
+let word = ["code", "tech","program","database", "software","software"]
+
+    for (let i = 0; i < prefix.length; i++) {
+        for (let j = 0; j < word.length; j++) {
+            console.log(prefix[i] + word[j] + '.com');
+        }
+    }
+    document.getElementById("domain").innerHTML = "Hello";
